634a61505116fb35203d3e5ae1e0b231c31be75d
MaximePig <maxime.pigueller@icloud.com>
2026-04-16 10:06:58 +0200

    change pictures to the ones we took outside and deleted any useless images

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -11,6 +11,8 @@ carbon_app=Blueprint('carbon_app',__name__)
 
 #Emissions factor per transport in kg per passemger km
 #Data from: http://efdb.apps.eea.europa.eu/?source=%7B%22query%22%3A%7B%22match_all%22%3A%7B%7D%7D%2C%22display_type%22%3A%22tabular%22%7D
+
+# CO2 emissions factor in kg per passenger km
 efco2={'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
     'Car':{'Petrol':0.18592,'Diesel':0.16453,'No Fossil Fuel':0},
     'Plane':{'Petrol':0.24298},
@@ -19,6 +21,8 @@ efco2={'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
     'Scooter':{'No Fossil Fuel':0},
     'Bicycle':{'No Fossil Fuel':0},
     'Walk':{'No Fossil Fuel':0}}
+
+# CO2 emissions factor in kg per passenger km
 efch4={'Bus':{'Diesel':2e-5,'CNG':2.5e-3,'Petrol':2e-5,'No Fossil Fuel':0},
     'Car':{'Petrol':3.1e-4,'Diesel':3e-6,'No Fossil Fuel':0},
     'Plane':{'Petrol':1.1e-4},
